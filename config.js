44b270a81f1253347fe6ef7e6decb076bdebda53
Yakccco <yangking105@gmail.com>
2024-02-05 11:46:07 +0800

    create aproject

---
+++ config.js
@@ -0,0 +1,52 @@
+// 2019年10月10日 https://github.com/AJLoveChina/birthday
+var config = {
+    // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
+    // 每句话尽量不要超过15个字,不然展示效果可能不太好
+    texts: [
+        "ที่รัก~",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Today you'll be 21 years old",  // 同上...
+        "Same age as me嘿嘿",
+        "In the new year I hope u",
+        "Better health and better grades",
+        "Graduate successfully!",
+        "And then find a good job",
+        "Anyway..",
+        "I love u forever",
+        "Always support u",
+        "TRUST YOU!",
+        "If u ask me why",
+        "I can only tell u",
+        "I LOVE YOU",
+        "You are a light in my life",
+        "I can't leave u",
+        "ที่รัก~",
+        "HAPPY BIRTHDAY",
+        "I give u my best wishes",
+        "爱你呀",
+        
+    ],
+    /**
+     * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
+     * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
+     * 例如
+     * "心爱的小可爱": "./imgs/xiaokeai.jpg"
+     *
+     * 如果不要图片的话, 直接在每行开头写两个斜杠注释即可, 例如下面的 "今天是你的生日" 的图片就不会展示了:)
+     * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
+     */
+    imgs: {
+        "心爱的小可爱": "./imgs/baby.png",
+        // "今天是你的生日": "./imgs/birthday.jpg",
+    },
+    // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
+    desc: {
+        turn_on: "turn on lights",
+        play: "start",
+        bannar_coming: "let's decorate",
+        balloons_flying: "something is missing",
+        cake_fadein: "most delicious cake ever",
+        light_candle: "light candle",
+        wish_message: "happy birthday",
+        story: "A MESSAGE FOR YOU",
+    }
+};
